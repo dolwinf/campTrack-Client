7492809ea5f7b646249ea017faf661273cd7e3d0
shell <dolwinf@gmail.com>
2020-01-15 19:51:22 +1100

    Removed service worker default file

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "../components/Header";
 import Map from "../components/Map";
-// import withRoot from "../withRoot";
 
 const App = () => {
   return (
